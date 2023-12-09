fdddeabe14365a38eb1672ce2c05106a382a4734
innovatedmonster <1219341322@qq.com>
2023-12-09 15:05:06 +0800

    set only see gpu3

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import argparse
 
 from utils.config import load
 from tools import cifar10_train, cifar10_train_daq
-
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
 def parse_args():
     parser = argparse.ArgumentParser(description='quantization network')
