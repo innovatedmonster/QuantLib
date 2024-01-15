4d6e2a2bc28b5e4b406382bd374ea8214027108c
innovatedmonster <1219341322@qq.com>
2024-01-15 11:26:22 +0800

    test wgt grad_input

--- models/quantization/qil/qil.py
+++ models/quantization/qil/qil.py
@@ -271,7 +271,7 @@ class FunTSF(torch.autograd.Function):
             grad_gamma = ((alpha * absol.apply(x) + beta) ** (gamma)) * \
                 torch.log(alpha * absol.apply(x) + beta) * torch.sign(x) # bug可能存在，韩国人没写γ的梯度
 
-            # print('\n---\nwgt\ngrad_input', grad_input)
+            print('\n---\nwgt\ngrad_input', grad_input)
             # print('grad_c_delta', grad_c_delta.sum(0))
             # print('grad_d_delta', grad_d_delta.sum(0))
             
