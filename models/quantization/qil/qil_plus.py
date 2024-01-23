9acb69a415cc0aa3987907e4a93ddf973a9d76f6
innovatedmonster <1219341322@qq.com>
2024-01-23 11:37:29 +0800

    fix back qil_plus wgt_soft_quant

--- models/quantization/qil/qil_plus.py
+++ models/quantization/qil/qil_plus.py
@@ -116,8 +116,6 @@ class QILPlusConv2d(nn.Conv2d):
         # delta = (u - l) / self.bit_range # delta就是scale
         # interval = (x - l) / delta
         interval = torch.pow((absol.apply(x) - l) / (u - l), gamma) * torch.sign(x) * self.bit_range_half
-        smaller = (x < 0).float()
-        interval = interval - smaller
         interval = torch.clamp(interval+self.bit_range_half, min=0, max=self.bit_range)
         output = 2 * self.soft_argmax(interval, self.wgt_temp, self.wgt_sigma) - self.bit_range
         return output / self.bit_range
